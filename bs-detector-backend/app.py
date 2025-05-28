228ed2bd5f35d4256d0499c29590a96ac5cd572a
Shamrat Neero <shamratneero@gmail.com>
2025-05-28 02:32:23 -0400

    Added rate limiter for extensive use

--- bs-detector-backend/app.py
+++ bs-detector-backend/app.py
@@ -3,6 +3,9 @@ from flask_cors import CORS
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 
 # Load environment variables
 load_dotenv()
@@ -11,10 +14,18 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["10 per minute"]
+)
+
+
 # Initialize OpenAI client
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 @app.route("/summarize", methods=["POST"])
+@limiter.limit("5 per minute")
 def summarize():
     data = request.get_json()
     text = data.get("text", "")
